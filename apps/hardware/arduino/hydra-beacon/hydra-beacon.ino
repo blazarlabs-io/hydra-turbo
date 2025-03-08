22cfff96412db34b5c17236a050775032ea90575
Fito Segrerav <fitosegrerav@gmail.com>
2025-03-08 18:18:14 -0500

    esp32 ble reconection logic implemented

--- apps/hardware/arduino/hydra-beacon/hydra-beacon.ino
+++ apps/hardware/arduino/hydra-beacon/hydra-beacon.ino
@@ -30,6 +30,17 @@ class MyCallbacks : public BLECharacteristicCallbacks {
   }
 };
 
+class MyServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+        Serial.println("Client connected!");
+    }
+
+    void onDisconnect(BLEServer* pServer) {
+        Serial.println("Client disconnected, restarting advertising...");
+        pServer->getAdvertising()->start();  // Restart BLE advertising
+    }
+};
+
 void setup() {
   // Start serialport for debug
   Serial.begin(115200);
@@ -43,6 +54,7 @@ void setup() {
   BLEDevice::init(DEVICE_NAME);
   // Create the BLE server
   BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
 
   // Create services
   BLEService *pService = pServer->createService(SERVICE_UUID);
@@ -69,10 +81,18 @@ void setup() {
   pService->start();
 
   //start advertising service
-  BLEAdvertising *pAdvertising = pServer->getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  // pAdvertising->addServiceUUID(SERVICE_UUID);
 
-  pAdvertising->start();
+  // pAdvertising->start();
+
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
 void loop() {
